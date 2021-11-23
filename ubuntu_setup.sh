8b4dcd2b16070bdad9d166a263f6559166298590
Nick Sorros <nsorros@gmail.com>
2021-11-23 09:47:15 +0000

    Add powerline

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -9,6 +9,17 @@ apt install gh
 apt install jq
 apt install aws_cli
 apt install direnv
+apt install powerflinw
 
 echo "# Direnv" >> ~/.bashrc
 echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+echo "" >> ~/.bashrc
+
+echo "# Powerline" >> ~/.bashrc
+echo "if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then" >> ~/.bashrc
+echo "    powerline-daemon -q" >> ~/.bashrc
+echo "    POWERLINE_BASH_CONTINUATION=1" >> ~/.bashrc
+echo "    POWERLINE_BASH_SELECT=1" >> ~/.bashrc
+echo "    source /usr/share/powerline/bindings/bash/powerline.sh" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
+echo "" >> ~/.bashrc
